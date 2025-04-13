4b3f4558f216a0241a0a82b4e32f771af52c084f
sdjge <32814572+sdjge@users.noreply.github.com>
2025-04-13 18:19:18 +0800

    fix tooltips

--- kubejs/client_scripts/toolipsToIcons.js
+++ kubejs/client_scripts/toolipsToIcons.js
@@ -1,62 +1,66 @@
 ItemEvents.tooltip((event) => {
 	let { player } = Client;
 
-	event.addAdvanced(
-		{ tag: "forge:armors" },
-		/**
-		 * @param {Internal.ItemStack} stack
-		 * @param {boolean} isAdvanced
-		 * @param {Internal.List<Internal.Component>} tooltip
-		 */
-		(stack, isAdvanced, tooltips) => {
-			if (!Client.isShiftDown()) {
-				tooltips.removeIf(($) => $.toString().includes("modifier") || Text.isEmpty($));
-				let comp = Text.empty();
-				if (player) {
-					let slot = onSlots(stack);
-					let equipped = player.getItemBySlot(slot);
-					let atrMap = stack.getAttributeModifiers(slot);
-					let otherMap = equipped.getAttributeModifiers(slot);
+	function tooltipAdv(tool_tag) {
+		event.addAdvanced(
+			{ tag: tool_tag },
+			/**
+			 * @param {Internal.ItemStack} stack
+			 * @param {boolean} isAdvanced
+			 * @param {Internal.List<Internal.Component>} tooltip
+			 */
+			(stack, isAdvanced, tooltips) => {
+				if (!Client.isShiftDown()) {
+					tooltips.removeIf(($) => $.toString().includes("modifier") || Text.isEmpty($));
+					let comp = Text.empty();
+					if (player) {
+						let slot = onSlots(stack);
+						let equipped = player.getItemBySlot(slot);
+						let atrMap = stack.getAttributeModifiers(slot);
+						let otherMap = equipped.getAttributeModifiers(slot);
 
-					let colors = Color.WHITE;
+						let colors = Color.WHITE;
 
-					atrMap.forEach((atr, modifier) => {
-						let vlaue = 0;
+						atrMap.forEach((atr, modifier) => {
+							let vlaue = 0;
 
-						atrMap.get(atr).forEach((amm) => {
-							vlaue += amm.amount;
-						});
-
-						if (!equipped.isEmpty() && !equ.equals(stack) && !otherMap.isEmpty()) {
-							let ovlaue = 0;
-
-							otherMap.get(atr).forEach((amm) => {
-								ovlaue += amm.amount;
+							atrMap.get(atr).forEach((amm) => {
+								vlaue += amm.amount;
 							});
 
-							if (vlaue != ovlaue) {
-								colors = vlaue > ovlaue ? Color.GREEN : Color.RED;
+							if (!equipped.isEmpty() && !equipped.equals(stack) && !otherMap.isEmpty()) {
+								let ovlaue = 0;
+
+								otherMap.get(atr).forEach((amm) => {
+									ovlaue += amm.amount;
+								});
+
+								if (vlaue != ovlaue) {
+									colors = vlaue > ovlaue ? Color.GREEN : Color.RED;
+								}
 							}
-						}
-						let { amount } = modifier;
-						if (amount) {
-							comp.append([
-								{
-									translatable: "kubejs." + atr.descriptionId,
-									font: "kubejs:icons",
-								},
-								Text.of(vlaue >= 1 ? vlaue.toFixed() : (vlaue * 100).toFixed() + "%").color(colors),
-								" ",
-							]);
-						}
-					});
-				}
-				if (!comp.isEmpty()) {
-					tooltips.add(1, comp);
+							let { amount } = modifier;
+							if (amount) {
+								comp.append([
+									{
+										translate: "kubejs." + atr.descriptionId,
+										font: "kubejs:icons",
+									},
+									Text.of(vlaue >= 1 ? vlaue.toFixed() : (vlaue * 100).toFixed() + "%").color(colors),
+									" ",
+								]);
+							}
+						});
+					}
+					if (!comp.isEmpty()) {
+						tooltips.add(1, comp);
+					}
 				}
 			}
-		}
-	);
+		);
+	}
+	tooltipAdv("forge:armors");
+	tooltipAdv("kjs:tools");
 });
 /**
  * @param {Internal.ItemStack} stack
